ca4df3af697627a9133b444d9c338c53a0b7ec24
thatherahere <mastanametal@gmail.com>
2018-12-21 18:43:13 +0530

    first commit

---
+++ .sfdx/typings/lwc/apex/AccountListController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/AccountListController.getRecentModifiedAccounts" {
+  export default function getRecentModifiedAccounts(): Promise<any>;
+}
---
+++ force-app/main/default/lwc/Accounts/Accounts.js
@@ -0,0 +1,68 @@
+import { LightningElement, track, wire, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+import getRecentModifiedAccounts from "@salesforce/apex/AccountListController.getRecentModifiedAccounts"
+
+
+// Fields to Display for Selected record in RecordForm
+const fields=['Name','AccountNumber','OwnerId','AccountSource','ParentId','AnnualRevenue','Type','CreatedById','LastModifiedById','Industry','Description','Phone'];
+
+const columns = [
+    {label: 'Account Name', fieldName: 'recordId', type: 'url', typeAttributes: { label:{fieldName:'Name'}, target: '_parent'}},
+    {label: 'Account Number', fieldName: 'AccountNumber', type: 'text'},
+    {label: 'Account Source', fieldName: 'AccountSource', type: 'text'},
+    {label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency', typeAttributes: { currencyCode: 'USD'}},
+    {label: 'Type', fieldName: 'Type', type: 'text'},
+    {label: 'Website', fieldName: 'Website', type: 'url', typeAttributes: { target: '_parent'}},
+    {label: 'Industry', fieldName: 'Industry', type: 'text'},
+    {label: 'Account Owner', fieldName: 'OwnerId', type: 'url', typeAttributes: { label:{fieldName:'OwnerName'}, target: '_parent'}},
+];
+
+export default class Accounts extends NavigationMixin(LightningElement) {
+    @api recordId;
+    @track accounts;
+    @track error;
+    @track fields = fields;
+    @track columns = columns;
+    @track maxRowSelection = 1;
+    @track mapMarkers = [];
+    @track zoomLevel=16;
+
+    @wire(getRecentModifiedAccounts)
+    wiredAccounts({error, data}){
+        if(data){
+            const rows = [];
+            data.forEach( function( account ){
+                // ES2015 (ES6) Object constructor: Object.assign | This new method allows to easily copy values from one object to another.
+                const accountObj = Object.assign({}, account, {
+                    recordId: '/lightning/r/'+account.Id+'/view',
+                    OwnerId: '/lightning/r/'+account.Owner.Id+'/view',
+                    OwnerName: account.Owner.Name,
+                });
+                rows.push(accountObj);
+            });
+            this.accounts = rows;
+        }else{
+            this.error = error; 
+        }
+    }
+
+    getSelectedAccount( event ){
+        const account = event.detail.selectedRows[0];
+        this.recordId = account.Id;
+        this.mapMarkers = [
+            {
+                location: {
+                    // Location Information
+                    City: account.BillingCity,
+                    Country: account.BillingCountry,
+                    PostalCode: account.BillingPostalCode,
+                    State: account.BillingState,
+                    Street: account.BillingStreet,
+                },
+
+                icon: 'standard:account',
+                title: account.Name
+            }
+        ];
+    }
+}
